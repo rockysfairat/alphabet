801a8f7411dbd57cd2bee3c9ecdb3914293ca2bc
Ålexander <yurchenko@post.com>
2022-04-12 17:53:05 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const alphabet = [
+  `<div class="character A">
+<!-- first row -->
+<div class="blackRectang"></div>
+<div class="blackRectang"></div>
+<div class="whiteRectang"></div>
+<div class="whiteRectang"></div>
+<div class="whiteRectang"></div>
+<div class="blackRectang"></div>
+<div class="blackRectang"></div>
+<!-- second row -->
+<div class="blackRectang"></div>
+<div class="whiteRectang"></div>
+<div class="whiteRectang"></div>
+<div class="whiteRectang"></div>
+<div class="whiteRectang"></div>
+<div class="whiteRectang"></div>
+<div class="blackRectang"></div>
+<!-- third row -->
+<div class="blackRectang"></div>
+<div class="whiteRectang"></div>
+<div class="blackRectang"></div>
+<div class="blackRectang"></div>
+<div class="whiteRectang"></div>
+<div class="whiteRectang"></div>
+<div class="blackRectang"></div>
+<!-- fourth row -->
+<div class="blackRectang"></div>
+<div class="whiteRectang"></div>
+<div class="blackRectang"></div>
+<div class="blackRectang"></div>
+<div class="whiteRectang"></div>
+<div class="whiteRectang"></div>
+<div class="blackRectang"></div>
+<!-- fifth row -->
+<div class="blackRectang"></div>
+<div class="whiteRectang"></div>
+<div class="whiteRectang"></div>
+<div class="whiteRectang"></div>
+<div class="whiteRectang"></div>
+<div class="whiteRectang"></div>
+<div class="blackRectang"></div>
+<!-- sixth row -->
+<div class="blackRectang"></div>
+<div class="whiteRectang"></div>
+<div class="blackRectang"></div>
+<div class="blackRectang"></div>
+<div class="whiteRectang"></div>
+<div class="whiteRectang"></div>
+<div class="blackRectang"></div>
+<!-- seventh row -->
+<div class="blackRectang"></div>
+<div class="whiteRectang"></div>
+<div class="blackRectang"></div>
+<div class="blackRectang"></div>
+<div class="whiteRectang"></div>
+<div class="whiteRectang"></div>
+<div class="blackRectang"></div>
+</div>`,
+  `text`,
+];
+
+function captureText() {
+  let textToDisplay = document.getElementById("mytext").value;
+  console.log(textToDisplay);
+}
